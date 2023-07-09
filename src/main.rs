eb6ec0649d9635691aa608dd3702b457f12fbd65
Mathias Kaempffer <ma.kaempffer@duocuc.cl>
2023-07-09 03:41:37 -0400

    Spawning entities at mouse position

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-
-use std::sync::mpsc::Receiver;
-
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_startup_system(startup_system)
+        .add_system(mouse_click_system)
         .add_system(state_system)
         .run();
 }
@@ -51,20 +49,71 @@ fn setup(
 
 }
 
-fn state_system(mut query: Query<&mut Transistor>) {
+fn spawn_component_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = q_windows.single();
+
+    let width = window.resolution.width();
+    let height = window.resolution.height();
+
+    let (x, y) = match window.position {
+        WindowPosition::At(v) => (v.x as f32, v.y as f32),
+        _ => (0., 0.),
+    };
+
+
+    if let Some(position) = q_windows.single().cursor_position() {
+        println!("Cursor is inside the primary window, at {:?}", position);
+        commands.spawn((
+            MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(Color::DARK_GRAY)),
+            transform: Transform::from_translation(Vec3::new(position.x - width/2.0, position.y - height / 2.0, 0.)),
+            
+            ..default()
+        },
+        Transistor { state: PowerState::Off }
+    ));
+    } else {
+        println!("Can't spawn if cursor is not in the game window.");
+    }
     
-    for mut transistor in &mut query {
-        println!("{:?}", transistor.state);
+}
+
+fn state_system(mut query: Query<&mut Transistor>) {
+    for transistor in &mut query {
         match transistor.state {
             PowerState::On => true,
             PowerState::Off => false,
         };
+    }
+}
 
-        if matches!(transistor.state, PowerState::Off) {
-            transistor.state = PowerState::On;
-        } else {
-            transistor.state = PowerState::Off;
-        }
+fn mouse_click_system(
+    commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    mouse_button_input: Res<Input<MouseButton>>
+) {
+    if mouse_button_input.pressed(MouseButton::Left) {
+
+        info!("left mouse currently pressed");
+    }
+
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        spawn_component_system(commands, meshes, materials, q_windows);
+        info!("left mouse just pressed");
+    }
+
+    if mouse_button_input.just_released(MouseButton::Left) {
+        info!("left mouse just released");
     }
 }
 
